4f8a17a63047274f966cb2a2a125c8fda8f6343a
Vlad <vladvr123@gmail.com>
2026-04-22 12:28:47 +0300

    Day 6

--- service/services/views.py
+++ service/services/views.py
@@ -8,31 +8,24 @@ from services.models import Subscription
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = (
-        Subscription.objects.all()
-        .prefetch_related(
-            "plan",
-            Prefetch(
-                "client",
-                queryset=Client.objects.all()
-                .select_related("user")
-                .only("company_name", "user__email"),
-            ),
-        )
-        .annotate(
-            price=F("service__full_price")
-            - F("service__full_price")
-            * F("plan__discount_percent") / 100.00
-        )
+    queryset = Subscription.objects.all().prefetch_related(
+        "plan",
+        Prefetch(
+            "client",
+            queryset=Client.objects.all()
+            .select_related("user")
+            .only("company_name", "user__email"),
+        ),
     )
-    '''annotate - вычисление на уровне базы'''
     serializer_class = SubscriptionSerializer
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
         response = super().list(request, *args, **kwargs)
 
-        response_data = {'result': response.data}
-        response_data['total_amount'] = queryset.aggregate(total=Sum('price')).get('total')
+        response_data = {"result": response.data}
+        response_data["total_amount"] = queryset.aggregate(total=Sum("price")).get(
+            "total"
+        )
         response.data = response_data
         return response
